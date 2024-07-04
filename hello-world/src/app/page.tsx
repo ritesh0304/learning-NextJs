76e916d2b734b701207ae7381de385ad8241ea32
ritesh0304 <ranaritesh0304@gmail.com>
2024-07-04 11:35:47 +0530

    Links in nextJs

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import Link from "next/link"
+import styles from './global.module.css'
 function Home() {
-  return (
-    <div>Home</div>
+  return (    
+    <>
+      <div className={styles.Home}>
+      <div className="">Home</div>
+    <Link className="" href="/blog">Go to Blog Page</Link>
+    <Link className="" href="/products" >Products</Link>
+      </div>
+    </>
   )
 }
 
--- hello-world/src/app/products/page.tsx
+++ hello-world/src/app/products/page.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
 
 export default function Products() {
   return (
     <>
+      <Link href="/"> Home </Link>
     <h1>Products page</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
+      <h2> <Link href='/products/1' >Product 1</Link></h2>
+      <h2> <Link href='/products/2/review/1' replace >Product 2</Link></h2>
       <h2>Product 3 </h2>
     </>
   )
